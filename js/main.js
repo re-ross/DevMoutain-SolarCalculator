0813e334f996d3d574b556887ea26f697cd052be
Ryan Ross <ryan@reross.com>
2022-03-01 13:21:35 -0600

    finished

--- js/main.js
+++ js/main.js
@@ -60,10 +60,39 @@ var sunHoursPerDay = sunHrs();
 var minKwNeeds = dailyUseKw / sunHoursPerDay
 var realKwNeeds = minKwNeeds * 1.25
 var realWattNeeds = realKwNeeds * 1000;
- var panelInfo = calculatePanel();
+var panelInfo = calculatePanel();
 var panelOutput = panelInfo[0];
 var panelName = panelInfo[1];
 console.log(panelName);
 var panelsNeed = Math.ceil(realWattNeeds / panelOutput)
 console.log(panelsNeed);
 }
+
+let feedback = "";
+  feedback +=
+    "<p> Based on your average daily use of " +
+    Math.round(dailyUseKw) +
+    " kWh, you will need to purchase " +
+    panelsNeeded +
+    " " +
+    panelName +
+    " solar panels to offset 100% of your electricity bill.</p>";
+  feedback += "<h2>Additional Details</h2>";
+  feedback +=
+    "<p> Your average daily elecricity consumption: " +
+    Math.round(dailyUseKw) +
+    " Kwh per day. </p>";
+  feedback +=
+    "<p>Average sunshine hours per day: " + sunHoursPerDay + " hours</p>";
+  feedback +=
+    "<p>Realistic watts needed per hour: " +
+    Math.round(realWattNeeds) +
+    " watts/hour.</p>";
+  feedback +=
+    "<p>the " +
+    panelName +
+    " panel you selected generates about " +
+    panelOutput +
+    " watts per hour. </p>";
+
+  document.getElementById("feedback").innerHTML = feedback;
